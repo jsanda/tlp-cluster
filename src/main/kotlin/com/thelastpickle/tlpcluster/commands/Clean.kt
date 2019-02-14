971138421b4bd64eb2d374bc0c4aa9929641423b
Jon Haddad <jon@jonhaddad.com>
2019-02-14 13:27:42 -0800

    improving cluster clean

--- src/main/kotlin/com/thelastpickle/tlpcluster/commands/Clean.kt
+++ src/main/kotlin/com/thelastpickle/tlpcluster/commands/Clean.kt
@@ -9,6 +9,22 @@ class Clean : ICommand {
         File("terraform.tfstate.backup").delete()
         File("stress_ips.txt").delete()
         File("hosts.txt").delete()
+        File("terraform.tf.json").delete()
+        File("terraform.tfvars").delete()
+        File("build_cassandra.sh").delete()
+
+        val toDelete = listOf("copy_provisioning_resources.sh",
+                "create_provisioning_resources.sh",
+                "docker-compose.yml",
+                "Dockerfile",
+                "DockerfileCassandra",
+                "DockerfileSSH"
+                )
+
+        for(f in toDelete) {
+            File(f).delete()
+        }
+        File(".terraform").deleteRecursively()
     }
 
 }
